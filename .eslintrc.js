60c89bf7bd47e55623b1c42ae9f393a9fb2a4d9d
Ricardo Martins <ricardo@ricardomartins.net.br>
2025-10-09 17:05:01 +1000

    chore: add ESLint and Prettier configs, fix lint script for npm publish

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  extends: ['plugin:n8n-nodes-base/community'],
+  rules: {},
+};
